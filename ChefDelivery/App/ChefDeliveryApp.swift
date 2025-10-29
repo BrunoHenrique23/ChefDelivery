24f61bcd4f2d400714f475deafb99f265a114449
BrunoHenrique23 <bhferrassilva@gmail.com>
2025-10-29 18:44:13 -0300

    Add files via upload

---
+++ ChefDelivery/App/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Bruno Henrique Ferraz da Silva on 29/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
